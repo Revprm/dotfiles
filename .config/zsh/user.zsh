3adbb3e621493ef35460b3c6634cc55227e707a1
revprm <revy.pramana@gmail.com>
2025-10-21 14:47:31 +0700

    fix: update fastfetch and kitty configurations for improved theming and layout

--- .config/zsh/user.zsh
+++ .config/zsh/user.zsh
@@ -5,9 +5,9 @@ if [[ $- == *i* ]]; then
     # This is a good place to load graphic/ascii art, display system information, etc.
     if command -v pokego >/dev/null; then
         pokego --no-title -r 1,3,6
-    elif command -v pokemon-colorscripts >/dev/null; then
+        elif command -v pokemon-colorscripts >/dev/null; then
         pokemon-colorscripts --no-title -r 1,3,6
-    elif command -v fastfetch >/dev/null; then
+        elif command -v fastfetch >/dev/null; then
         if do_render "image"; then
             fastfetch --logo-type kitty
         fi
@@ -22,9 +22,9 @@ unset HYDE_ZSH_PROMPT
 if [[ ${HYDE_ZSH_NO_PLUGINS} != "1" ]]; then
     #  OMZ Theme 
     # Set the Oh My Zsh theme you want to use.
-    ZSH_THEME="gruvbox"
+    ZSH_THEME="onedark"
     SOLARIZED_THEME="dark"
-
+    
     #  OMZ Plugins 
     # manually add your oh-my-zsh plugins here
     plugins=(
